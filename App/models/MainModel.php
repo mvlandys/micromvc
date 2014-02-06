eb28a0e2c3d5e03b413bbc9758b904b9264d0a88
Mathew Vlandys <mvlandys@gmail.com>
2014-02-06 14:30:25 +1100

    Added bower functionality

--- App/models/MainModel.php
+++ App/models/MainModel.php
@@ -1,6 +1,8 @@
 <?php
     namespace Matheos\App;
 
+    use Matheos\MicroMVC\AppConfig;
+
     class MainModel extends \Matheos\MicroMVC\Model {
         public function get_AppLibs( $classname ) {
             $js_files  = array("common.js", "Alert.js",  "Error.js",  "{$classname}.js");
@@ -23,26 +25,46 @@
             return $htmlData;
         }
 
+        /**
+         * Reads the root bower.json file and iterates through all the dependencies
+         * found in App/lib/vendor
+         *
+         * Reads the dependency's bower.json file to get an array of files to add
+         *
+         * Determines if the file is a CSS or JS file and adds it to the relevant array.
+         *
+         * return array of minified js/css file strings
+         */
         public function get_VendorLibs( $classname ) {
-            $css_files = array(
-                "Bootstrap/css/bootstrap.css",
-                "Colorbox/colorbox.css",
-                "JQuery-UI/css/jquery-ui-1.9.2.custom.css",
-                "Chosen-JS/chosen.css"
-            );
+            $appCfg = AppConfig::getInstance()->config;
+            $bower  = json_decode( file_get_contents($appCfg->Core->rootFolder . "/bower.json") );
+            $css    = array();
+            $js     = array();
 
-            $js_files  = array(
-                "JQuery/jquery.min.js",
-                "Bootstrap/js/bootstrap.js",
-                "JQuery-UI/js/jquery-ui-1.10.2.custom.js",
-                "Colorbox/jquery.colorbox.js",
-                "JQuery/jquery.form.js",
-                "Chosen-JS/chosen.jquery.min.js"
-            );
+            foreach($bower->dependencies as $lib=>$ver) {
+                $path = $appCfg->Core->rootFolder . "/App/lib/vendor/" . $lib . "/";
+
+                if (! file_exists($path . "bower.json")) {
+                    continue;
+                }
+
+                $lib_bower = json_decode( file_get_contents($path . "bower.json") );
+                $files     = (is_array($lib_bower->main)) ? $lib_bower->main : array($lib_bower->main);
+
+                foreach($files as $file) {
+                    if (substr($file, strlen($file) - 3) == "css") {
+                        $css[] = $lib . "/" . $file;
+                    }
+
+                    if (substr($file, strlen($file) - 2) == "js") {
+                        $js[] = $lib . "/" . $file;
+                    }
+                }
+            }
 
             $htmlData = array(
-                "js_vendor"   => $this->minifyString($js_files),
-                "css_vendor"  => $this->minifyString($css_files)
+                "js_vendor"   => $this->minifyString($js),
+                "css_vendor"  => $this->minifyString($css)
             );
 
             return $htmlData;
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
     require 'vendor/autoload.php';
-    //require_once("MicroMVC/Autoloader.php");
     require_once("Config/config.php");
 
     $router = new \Matheos\MicroMVC\Router();
