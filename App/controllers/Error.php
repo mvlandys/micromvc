10eab9951066a24c3b63798e56179c3756363a5f
Mathew Vlandys <mvlandys@gmail.com>
2014-04-10 12:33:50 +1000

    Error controller - code cleanup and comments

--- App/controllers/Error.php
+++ App/controllers/Error.php
@@ -1,36 +1,66 @@
 <?php
 	namespace Matheos\App;
 
-	class Error extends \Matheos\MicroMVC\Controller {
-        public function errorHandler($errno, $errstr, $errfile, $errline) {
-            echo "<br/><b>Error :</b> <i>$errstr</i><br/><b>Line $errline:</b> $errfile<br/>";
+    use Matheos\MicroMVC\AppConfig;
+    use Matheos\MicroMVC\Controller;
 
-            $AppConfig = \Matheos\MicroMVC\AppConfig::getInstance();
-            $LogErrors = $AppConfig->config->Core->logErrors;
+	class Error extends Controller
+    {
+        /*
+         *  Custom Error Method
+         *
+         *  Outputs the error and saves error to the db
+         *
+         *  @param int $errno
+         *  @param string $errstr
+         *  @param string $errfile
+         *  @param int $errline
+         */
+        public function errorHandler($errno, $errstr, $errfile, $errline)
+        {
+            $appCfg    = AppConfig::getInstance();
+            $logErrors = $appCfg->config->Core->logErrors;
 
-            if ($LogErrors == "true") {
+            if ($logErrors == "true") {
                 $errorID = $this->model->insert_Error($errstr, $errno);
                 echo "<b>Error ID:</b> " . $errorID;
+            } else {
+                echo "<br/><b>Error :</b> <i>$errstr</i><br/><b>Line $errline:</b> $errfile<br/>";
             }
 
             exit(1);
         }
 
-        public function exceptionHandler($e) {
-            echo "<br/><b>Exception :</b> <i>" . $e->getMessage() . "</i><br/>";
+        /*
+         *  Custom Exception Method
+         *
+         *  Outputs the exception and saves details to the db
+         *
+         *  @param \Exception $e
+         */
+        public function exceptionHandler($e)
+        {
+            $appCfg    = AppConfig::getInstance();
+            $logErrors = $appCfg->config->Core->logErrors;
 
-            $AppConfig = \Matheos\MicroMVC\AppConfig::getInstance();
-            $LogErrors = $AppConfig->config->Core->logErrors;
-
-            if ($LogErrors == "true") {
+            if ($logErrors == "true") {
                 $errorID = $this->model->insert_Error($e->getMessage(), $e->getCode(), $e->getTrace());
                 echo "<b>Error ID:</b> " . $errorID;
+            } else {
+                echo "<br/><b>Exception :</b> <i>" . $e->getMessage() . "</i><br/>";
             }
 
             exit(1);
         }
 
-        public function shutdownHandler() {
+        /*
+         *  Shutdown Error Method
+         *
+         *  Outputs the error and saves error to the db
+         *
+         */
+        public function shutdownHandler()
+        {
             $error = error_get_last();
             if (!empty($error)) {
                 $errno   = $error["type"];
@@ -43,21 +73,29 @@
                     "file"  => $errfile
                 ));
 
-                echo "<br/><b>Fatal error :</b> <i>$errstr</i><br/><b>Line $errline:</b> $errfile<br/>";
+                $appCfg    = AppConfig::getInstance();
+                $logErrors = $appCfg->config->Core->logErrors;
 
-                $AppConfig = \Matheos\MicroMVC\AppConfig::getInstance();
-                $LogErrors = $AppConfig->config->Core->logErrors;
-
-                if ($LogErrors == "true") {
+                if ($logErrors == "true") {
                     $errorID = $this->model->insert_Error($errstr, $errno, $trace);
                     echo "<b>Error ID:</b> " . $errorID;
+                } else {
+                    echo "<br/><b>Fatal error :</b> <i>$errstr</i><br/><b>Line $errline:</b> $errfile<br/>";
                 }
             }
         }
 
-        public function showError( $ErrorID ) {
+        /*
+         *  Show Error View Method
+         *
+         *  Renders the view-error page
+         *
+         *  @param int $errorId
+         */
+        public function showError($errorId)
+        {
             $ErrorModel = new ErrorModel();
-            $Error      = $ErrorModel->get_Error( $ErrorID );
+            $Error      = $ErrorModel->get_Error($errorId);
 
             $this->renderSite("error.php", $Error);
         }
